f31ed82c42acf561acc381ea0c6fe84b356dd52d
Trang Do <trang.do@slalom.com>
2019-07-08 09:38:16 -0400

    Observables and component conversation

--- devlogger/src/app/components/log-form/log-form.component.ts
+++ devlogger/src/app/components/log-form/log-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LogService } from '../../services/log.service';
+import { Log } from '../../models/Log';
 
 @Component({
   selector: 'app-log-form',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-form.component.css']
 })
 export class LogFormComponent implements OnInit {
+  id: string;
+  text: string;
+  date: any;
 
-  constructor() { }
+  constructor(private logService: LogService) { }
 
   ngOnInit() {
+    // Subscribe to the selected log observable
+    this.logService.selectedLog.subscribe(log => {
+      if (log.id !== null) {
+        this.id = log.id;
+        this.text = log.text;
+        this.date = log.date;
+      }
+    })
   }
 
 }
--- devlogger/src/app/services/log.service.ts
+++ devlogger/src/app/services/log.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observation, Observable } from 'rxjs';
+import { Observable, BehaviorSubject, of } from 'rxjs';
 import { Log } from '../models/Log';
 
 @Injectable({
@@ -7,7 +7,9 @@ import { Log } from '../models/Log';
 })
 export class LogService {
   logs: Log[];
-
+  private logSource = new BehaviorSubject<Log>({id: null, text: null, date: null});
+  selectedLog = this.logSource.asObservable();
+  
   constructor () {
     this.logs = [
       {
@@ -28,7 +30,11 @@ export class LogService {
     ]
   }
 
-  getLogs() {
-    return this.logs
+  getLogs(): Observable<Log[]> {
+    return of(this.logs);
+  }
+
+  setFormLog(log: Log) {
+    this.logSource.next(log);
   }
 }

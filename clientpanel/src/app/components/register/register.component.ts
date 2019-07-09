813c59126d521a813275029653b0d6902949dd03
Trang Do <quynhtrangdo94@gmail.com>
2019-07-09 11:14:35 -0400

    Handled user registration

--- clientpanel/src/app/components/register/register.component.ts
+++ clientpanel/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor (
+    private authService: AuthService,
+    private _flashMessagesService: FlashMessagesService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    this.authService.register(this.email, this.password)
+      .then(res => {
+        this._flashMessagesService.show('You are now registered and logged in', { cssClass: 'alert-success' });
+        this.router.navigate(['/'])
+      })
+      .catch(err => {
+        this._flashMessagesService.show(err.message, { cssClass: 'alert-danger' });
+      })
+  }
 }

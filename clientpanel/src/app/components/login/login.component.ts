859114af8b4f511bfc9079099f06ba82519856b8
Trang Do <quynhtrangdo94@gmail.com>
2019-07-09 10:21:34 -0400

    Handled login authentication

--- clientpanel/src/app/components/login/login.component.ts
+++ clientpanel/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor (
+    private authService: AuthService,
+    private _flashMessagesService: FlashMessagesService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(auth => {
+      if (auth) {
+        this.router.navigate(['/'])
+      }
+    })
   }
 
+  onSubmit() {
+    this.authService.login(this.email, this.password)
+      .then(res => {
+        this._flashMessagesService.show('You are logged in', { cssClass: 'alert-success' });
+        this.router.navigate(['/'])
+      })
+      .catch(err => {
+        this._flashMessagesService.show(err.message, { cssClass: 'alert-danger' });
+        this.router.navigate(['/login']);
+      })
+  }
 }

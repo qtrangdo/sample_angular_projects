2500de75dbd4b671502c6c7e71e6a56f21896b97
Trang Do <quynhtrangdo94@gmail.com>
2019-07-09 10:52:12 -0400

    Added auth guard for routes

--- clientpanel/src/app/app-routing.module.ts
+++ clientpanel/src/app/app-routing.module.ts
@@ -10,19 +10,22 @@ import { RegisterComponent } from './components/register/register.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
+import { AuthGuard } from './components/guards/auth.guard';
+
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: 'client/add', component: AddClientComponent },
-  { path: 'client/edit/:id', component: EditClientComponent },
-  { path: 'client/:id', component: ClientDetailsComponent },
+  { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'client/add', component: AddClientComponent, canActivate: [AuthGuard] },
+  { path: 'client/edit/:id', component: EditClientComponent, canActivate: [AuthGuard] },
+  { path: 'client/:id', component: ClientDetailsComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'settings', component: SettingsComponent },
+  { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] },
   { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
---
+++ clientpanel/src/app/components/guards/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router
+  ) {}
+
+  canActivate(): Observable<boolean> {
+    return this.afAuth.authState.pipe(
+      map(auth => {
+        if(!auth) {
+          this.router.navigate(['/login']);
+          return false;
+        } else {
+          return true;
+        }
+      })
+    )
+  }
+}

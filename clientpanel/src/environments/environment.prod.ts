332ced4875ab554d990499d57232e6fffcb9f7ff
Trang Do <quynhtrangdo94@gmail.com>
2019-07-09 13:38:18 -0400

    Configure environment

--- clientpanel/src/environments/environment.prod.ts
+++ clientpanel/src/environments/environment.prod.ts
@@ -1,12 +1,14 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCqQHz38SMlNoIH9FLu9CZcv1pjuqdk7vI",
-    authDomain: "clientpanelprod-ba4d6.firebaseapp.com",
-    databaseURL: "https://clientpanelprod-ba4d6.firebaseio.com",
-    projectId: "clientpanelprod-ba4d6",
-    storageBucket: "clientpanelprod-ba4d6.appspot.com",
-    messagingSenderId: "215147131788",
-    appId: "1:215147131788:web:7109758fab948a74"
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
   }
 };
+
+//Enter firebase settings
